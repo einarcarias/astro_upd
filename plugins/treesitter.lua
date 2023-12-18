54136a10228f8957c5b168f3353a916385d5d23d
einar <einarcarias@gmail.com>
2023-12-18 13:16:53 +0000

    added latex

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -5,5 +5,14 @@ return {
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
       -- "lua"
     })
+    if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "bibtex", "latex" })
+      end
+    if type(opts.highlight.disable) == "table" then
+      vim.list_extend(opts.highlight.disable, { "latex" })
+    else
+      opts.highlight.disable = { "latex" }
+    end
+
   end,
 }
