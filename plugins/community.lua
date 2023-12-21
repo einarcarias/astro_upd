322cc7ed310fe610ccc6a282f555ec45ab0232ab
einar <einarcarias@gmail.com>
2023-12-21 12:30:07 +0000

    added a few plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -10,22 +10,24 @@ return {
   { import = "astrocommunity.editing-support.vim-move" },
   { import = "astrocommunity.motion.leap-nvim" },
   { import = "astrocommunity.motion.mini-surround" },
-  {import = "astrocommunity.markdown-and-latex.vimtex"},
+  { import = "astrocommunity.markdown-and-latex.vimtex" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.programming-language-support.csv-vim" },
+  { import = "astrocommunity.editing-support.yanky-nvim" },
   {
-  "lervag/vimtex",
-  lazy = false, -- lazy-loading will disable inverse search
-  config = function()
-    vim.api.nvim_create_autocmd({ "FileType" }, {
-      group = vim.api.nvim_create_augroup("lazyvim_vimtex_conceal", { clear = true }),
-      pattern = { "bib", "tex" },
-      callback = function()
-        vim.wo.conceallevel = 2
-      end,
-    })
+    "lervag/vimtex",
+    lazy = false, -- lazy-loading will disable inverse search
+    config = function()
+      vim.api.nvim_create_autocmd({ "FileType" }, {
+        group = vim.api.nvim_create_augroup("lazyvim_vimtex_conceal", { clear = true }),
+        pattern = { "bib", "tex" },
+        callback = function() vim.wo.conceallevel = 2 end,
+      })
 
-    vim.g.vimtex_mappings_disable = { ["n"] = { "K" } } -- disable `K` as it conflicts with LSP hover
-    vim.g.vimtex_quickfix_method = vim.fn.executable("pplatex") == 1 and "pplatex" or "latexlog"
-  end,
-},
+      vim.g.vimtex_mappings_disable = { ["n"] = { "K" } } -- disable `K` as it conflicts with LSP hover
+      vim.g.vimtex_quickfix_method = vim.fn.executable "pplatex" == 1 and "pplatex" or "latexlog"
+    end,
+  },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
