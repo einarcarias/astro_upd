0b6cb2e9c1feae3acd97fc24e7fcc3f654fff511
einar <einarcarias@gmail.com>
2023-12-24 19:21:31 +0000

    working luasnip setup

--- plugins/luasnip.lua
+++ plugins/luasnip.lua
@@ -1,18 +1,31 @@
 return {
-  "L3MON4D3/LuaSnip",
-  dependencies = { "rafamadriz/friendly-snippets" },
-  version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-  -- install jsregexp (optional!).
-  build = "make install_jsregexp",
-  config = function(plugin, opts)
-    -- include the default astronvim config that calls the setup call
-    require "plugins.configs.luasnip"(plugin, opts)
-    -- load snippets paths
-    require("luasnip.loaders.from_vscode").lazy_load {
-      -- this can be used if your configuration lives in ~/.config/nvim
-      -- if your configuration lives in ~/.config/astronvim, the full path
-      -- must be specified in the next line
-      paths = { "./lua/user/snippets" },
-    }
-  end,
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = { "rafamadriz/friendly-snippets" },
+    version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+    -- install jsregexp (optional!).
+    build = "make install_jsregexp",
+    config = function(plugin, opts)
+      -- include the default astronvim config that calls the setup call
+      require "plugins.configs.luasnip"(plugin, opts)
+      -- load snippets paths
+      -- add more custom luasnip configuration such as filetype extend or custom snippets
+      --     require "plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
+      --     -- add more custom luasnip configuration such as filetype extend or custom snippets
+      local luasnip = require "luasnip"
+      --     luasnip.filetype_extend("javascript", { "javascriptreact" })
+      luasnip.config =
+        {
+          set_configs = {
+            enable_autosnippets = true,
+            store_selection_keys = "<Tab>",
+          },
+        }, require("luasnip.loaders.from_lua").lazy_load {
+          -- this can be used if your configuration lives in ~/.config/nvim
+          -- if your configuration lives in ~/.config/astronvim, the full path
+          -- must be specified in the next line
+          paths = { "~/.config/nvim/lua/user/snippets/" },
+        }
+    end,
+  },
 }
