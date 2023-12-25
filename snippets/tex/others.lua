efe283b619acf0606fe62c376e86e45c355023c6
einar <einarcarias@gmail.com>
2023-12-25 00:57:37 +0000

    snippets

--- snippets/tex/others.lua
+++ snippets/tex/others.lua
@@ -1,4 +1,7 @@
 return {
 
   s({ trig = "cref", dscr = "Expands 'cref' into '\\cref{}'" }, fmta([[\cref{<>}]], { i(1) })),
+  -- units package in latex snippets
+  s({ trig = "qty", dscr = "Expands 'qty' into '\\qty{}{}'" }, fmta([[\qty{<>}{<>}]], { i(1), i(2) })),
+  s({ trig = "num", dscr = "Expands 'num' into '\\num{}{}'" }, fmta([[\num{<>}]], { i(1) })),
 }
