50276cd83c31a1edf891805cd6901588acab298e
einar <einarcarias@gmail.com>
2023-12-29 14:52:29 +0000

    changes on texlab

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -5,10 +5,9 @@ return {
     "williamboman/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = function(_, opts)
-      -- add more things to the ensure_installed table protecting against community packs modifying it
+      -- add more: things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "lua_ls",
-        -- "texlab",
+        "texlab",
         "ltex",
         -- "prettier"
       })
