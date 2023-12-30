192ae243411690855fd7c06837cb58852e6baa98
einar <einarcarias@gmail.com>
2023-12-30 01:02:51 +0000

    some changes

--- plugins/community.lua
+++ plugins/community.lua
@@ -35,7 +35,7 @@ return {
         callback = function() vim.wo.conceallevel = 2 end,
       })
 
-      vim.g.vimtex_mappings_disable = { ["n"] = { "K" } } -- disable `K` as it conflicts with LSP hover
+      -- vim.g.vimtex_mappings_disable = { ["n"] = { "K" } } -- disable `K` as it conflicts with LSP hover
       vim.g.vimtex_quickfix_method = vim.fn.executable "pplatex" == 1 and "pplatex" or "latexlog"
     end,
   },
