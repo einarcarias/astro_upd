f68a2aca1ba56a176f8a23d757856ccf60f902f0
einar <einarcarias@gmail.com>
2024-01-02 00:03:21 +0000

    change some stuff

--- plugins/autopairs.lua
+++ plugins/autopairs.lua
@@ -6,35 +6,41 @@ return {
     local npairs = require "nvim-autopairs"
     local Rule = require "nvim-autopairs.rule"
     local cond = require "nvim-autopairs.conds"
-    npairs.add_rules(
-      {
-        Rule("$", "$", { "tex", "latex" })
-          -- don't add a pair if the next character is %
-          :with_pair(cond.not_after_regex "%%")
-          -- don't add a pair if  the previous character is xxx
-          :with_pair(cond.not_before_regex("xxx", 3))
-          -- don't move right when repeat character
-          :with_move(cond.none())
-          -- don't delete if the next character is xx
-          :with_del(cond.not_after_regex "xx")
-          -- disable adding a newline when you press <cr>
-          :with_cr(cond.none()),
-      },
-      -- disable for .vim files, but it work for another filetypes
-      Rule("a", "a", "-vim"),
-      {
-        -- rule for autopairs to not close when \ is added
-        Rule("\\((%w*) $", "tex")
-          :replace_endpair(function(opts)
-            local beforeText = string.sub(opts.line, 0, opts.col)
-            local _, _, match = beforeText:find "\\((%w*)"
-            if match and #match > 0 then return " \\)" .. match end
-            return ""
-          end)
-          :with_move(cond.none())
-          :use_key("<space>")
-          :use_regex(true),
-      }
-    )
+    npairs.add_rules {
+      Rule("\\(", "\\)", { "tex", "latex" })
+        --       -- don't add a pair if the next character is %
+        :with_pair(cond.not_after_regex "%%")
+        --       -- don't add a pair if  the previous character is xxx
+        --       :with_pair(cond.not_before_regex("xxx", 3))
+        --       -- don't move right when repeat character
+        :with_move(
+          cond.none()
+        )
+        --       -- don't delete if the next character is xx
+        --       :with_del(cond.not_after_regex "xx")
+        --       -- disable adding a newline when you press <cr>
+        :with_cr(
+          cond.none()
+        ),
+    }
+    npairs.add_rules {
+      Rule("$", "$", { "tex", "latex" })
+        --       -- don't add a pair if the next character is %
+        :with_pair(cond.not_after_regex "%%")
+        --       -- don't add a pair if  the previous character is xxx
+        --       :with_pair(cond.not_before_regex("xxx", 3))
+        --       -- don't move right when repeat character
+        :with_move(
+          cond.none()
+        )
+        --       -- don't delete if the next character is xx
+        --       :with_del(cond.not_after_regex "xx")
+        --       -- disable adding a newline when you press <cr>
+        :with_cr(
+          cond.none()
+        ),
+    }
+    npairs.remove_rule("`", "tex")
+    npairs.remove_rule("(", "tex")
   end,
 }
