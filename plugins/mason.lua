e83d352984777334d1abaf2406bcc07862642e80
einarcarias <einarcarias@gmail.com>
2024-01-04 12:37:46 +0000

    make sure help works

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -9,6 +9,7 @@ return {
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
         "texlab",
         "ltex",
+        "lua_ls",
         -- "prettier"
       })
     end,
--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -3,7 +3,9 @@ return {
   opts = function(_, opts)
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-      -- "lua"
+      "lua",
+      "vimdoc",
+      "jsdoc",
     })
     if type(opts.ensure_installed) == "table" then vim.list_extend(opts.ensure_installed, { "bibtex", "latex" }) end
     if type(opts.highlight.disable) == "table" then
