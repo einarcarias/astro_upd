794c7bf68455e054d4da5d71db0d43173fcee431
einarcarias <einarcarias@gmail.com>
2024-01-10 17:09:27 +0000

    autopairs

--- plugins/community.lua
+++ plugins/community.lua
@@ -52,7 +52,7 @@ return {
         },
         suggestion = {
           enabled = true,
-          auto_trigger = false,
+          auto_trigger = true,
           debounce = 75,
         },
       }
